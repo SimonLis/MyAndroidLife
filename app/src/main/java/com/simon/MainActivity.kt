1660da5f9dff42baacf1c51e6b531c221c159b84
Simonlis <527355213@qq.com>
2020-03-24 11:14:46 +0800

    my delete no use files

--- app/src/main/java/com/simon/MainActivity.kt
+++ app/src/main/java/com/simon/MainActivity.kt
@@ -3,9 +3,11 @@ package com.simon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
